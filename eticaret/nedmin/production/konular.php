c14b201233ddf64eab83df452ae9bd0fe5901631
nisailgunn <nisailgyn@gmail.com>
2021-01-17 18:03:05 +0300

    ilk gönderme

---
+++ eticaret/nedmin/production/konular.php
@@ -0,0 +1,126 @@
+
+
+*-ÜYE GİRİŞ ÇIKIŞ İŞLEMLERİ => TAMAMLANDI
+*-MENÜ İŞLEMLERİ => TAMAMLANDI
+*-SLİDER MODÜLÜ => TAMAMLANDI
+*-KATEGORİ İŞLEMLERİ
+*-ÜRÜN İŞLEMLERİ
+*-Son gezilen ürünler modülü
+*-YORUM İŞLEMLERİ
+*-SEPET İŞLEMLERİ
+*-SİPARİŞ İŞLEMLERİ
+*-ÜRÜN ARAMA İŞLEMLERİ
+
+
+Ürün Sütunları
+
+urun_id
+kategori_id
+urun_zaman
+urun_ad
+urun_seourl
+urun_detay
+urun_fiyat
+urun_video
+urun_keyword
+urun_stok
+urun_durum
+
+
+ÖDEVLER
+
+*-Slider düzenle ve slider sil işlemleri
+*- Hesabim.php sayfasından giriş yapan kullanıcının bilgilerini düzenlemesini sağlayın...
+
+6.HAFTA ÖDEV
+
+*-ürün durumun yanına bir sutun açıp buton ekleyeceksiniz 
+"Öne çıkar" => butona tıklayınca o ürün öne çıkacak ve orda buton değişecek.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*-ÜYE GİRİŞ ÇIKIŞ İŞLEMLERİ => TAMAMLANDI
+*-MENÜ İŞLEMLERİ => TAMAMLANDI
+*-SLİDER MODÜLÜ => TAMAMLANDI
+*-KATEGORİ İŞLEMLERİ
+*-ÜRÜN İŞLEMLERİ
+*-SEPET İŞLEMLERİ
+*-SİPARİŞ İŞLEMLERİ
+*-ÜRÜN ARAMA İŞLEMLERİ
+
+
+ÖDEVLER
+
+*-Slider düzenle ve slider sil işlemleri
+*- Hesabim.php sayfasından giriş yapan kullanıcının bilgilerini düzenlemesini sağlayın...
